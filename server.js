dc8e9b660b3b9a798b5e619caea9a005805cbae0
Ryan Engle <rdengle@gmail.com>
2016-04-23 17:31:20 -0400

    Add POST for todos

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 5000;
-var todos = [{
-    id: 1,  // id for database (future)
-    description: 'Celebrate Kristen\'s new job',
-    completed: false
-}, {
-    id: 2,
-    description: 'Fix shower door',
-    completed: false
-}, {
-    id: 3,
-    description: 'Secure tree',
-    complete: true
-}];
+// hard-coded todos
+// var todos = [{
+//     id: 1,  // id for database (future)
+//     description: 'Celebrate Kristen\'s new job',
+//     completed: false
+// }, {
+//     id: 2,
+//     description: 'Fix shower door',
+//     completed: false
+// }, {
+//     id: 3,
+//     description: 'Secure tree',
+//     complete: true
+// }];
 
-// simple get request
-app.get('/', function(req, res) {
-    res.send('Todo API root');
-});
+// // simple get request
+// app.get('/', function(req, res) {
+//     res.send('Todo API root');
+// });
+var todos = [];
+var todoNextId = 1;
+
+// set up middleware
+// can now access json request through req.body 
+app.use(bodyParser.json());
 
 // HTTP method: GET /todos
 app.get('/todos', function(req, res) {    
@@ -45,6 +53,20 @@ app.get('/todos/:id', function(req, res) {
         res.status(404).send();        
     }
 });
+
+// HTTP method: POST /todos (all todos)
+app.post('/todos', function(req, res) {
+    var body = req.body; // via body-parser
+    
+    console.log('description: ' + body.description);
+    body.id = todoNextId++;
+    todos.push(body);
+    
+    res.json(body);
+});
+
+// HTTP method: POST ?? 
+
 
 // listen
 app.listen(PORT, function() {
