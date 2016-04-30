0adc188952d3ef2dc736d7173ee52231da4e4032
Ryan Engle <rdengle@gmail.com>
2016-04-29 23:11:06 -0400

    Adding DELETE /users/login

--- db.js
+++ db.js
@@ -3,14 +3,15 @@ var sequelize = new Sequelize(undefined, undefined, undefined, {
     'dialect': 'sqlite',  // type of database
     // storage location
     'storage': __dirname + '/data/dev-todo-api.sqlite'
-        // will fail if directory does not exist, thus use .keep for git
-    
+        // will fail if directory does not exist, thus use .keep for git    
 });
 
 var db = {};
 
-db.todo = sequelize.import(__dirname + '/models/todo.js');
-db.user = sequelize.import(__dirname + '/models/user.js');
+db.todo  = sequelize.import(__dirname + '/models/todo.js');
+db.user  = sequelize.import(__dirname + '/models/user.js');
+db.token = sequelize.import(__dirname + '/models/token.js');
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
--- middleware.js
+++ middleware.js
@@ -1,14 +1,31 @@
+var cryptojs = require('crypto-js');
+
 module.exports = function (db) {    
     return {
         requireAuthentication: function (req, res, next) {
             // get token from header
-            var token = req.get('Auth');
+            var token = req.get('Auth') || '';
             
-            db.user.findByToken(token).then(function (user) {
-                req.user = user;
-                next();
-            }, function (e) {
-                res.status(401).send();   
+            // look for token in db (created during login)
+            db.token.findOne({
+               where: {
+                   // from user's header
+                   tokenHash: cryptojs.MD5(token).toString() 
+               } 
+            }).then(function (tokenInstance) {
+                // if found in db
+                if (!tokenInstance){
+                    throw new Error();
+                }                
+                
+                req.token = tokenInstance;
+                return db.user.findByToken(token);                
+                
+            }).then(function (user) {
+                req.user = user;  
+                next();              
+            }).catch(function () {
+                res.status(401).send();
             });
         }  
     };
